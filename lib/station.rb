00ac1380c5f637d2c9c110857e233850d96fd12d
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-22 14:17:21 +0100

    Added test to check for zone attribute

--- lib/station.rb
+++ lib/station.rb
@@ -1,9 +1,10 @@
 
 
 class Station
-  attr_reader :name
+  attr_reader :name, :zone
 
-  def initialize(name = "Unnamed Station")
+  def initialize(name = "Unnamed Station", zone = "Undefined")
     @name = name
+    @zone = zone
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,4 +4,8 @@ describe Station do
   it "has a name" do
     expect(subject.name).to eq "Unnamed Station"
   end
+
+  it "has a zone" do
+    expect(subject.zone).to eq "Undefined"
+  end
 end
